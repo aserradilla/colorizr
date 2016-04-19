3468f44af960244b97c5c3402274cebc99526ae3
Alejandro Serradilla <alexserradillarufino@gmail.com>
2016-04-19 16:23:29 +0200

    Added colorizr.rb

---
+++ colorizr/lib/colorizr.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |s|
+        s.name        = 'colorizr'
+        s.version     = '0.0.1'
+        s.date        = '2016-04-19'
+        s.summary     = "Colors Everywhere!"
+        s.description = "A simple gem to colorize terminal output"
+        s.authors     = ["Alejandro Serradilla Rufino"]
+        s.files       = ["lib/colorizr.rb"]
+end
---
+++ colorizr/lib/colorizr.rb
@@ -0,0 +1,38 @@
+class String
+        attr_reader :colors_hash
+
+        # Hash that contains all the colors and its value
+        @@colors_hash = {
+                red: 31,
+                green: 32,
+                yellow: 33,
+                blue: 34,
+                pink: 35,
+                light_blue: 94,
+                white: 39,
+                light_grey: 90,
+                black: 30
+        }
+
+        def self.create_colors
+                @@colors_hash.keys.each do |key, value|
+                        self.class.send :define_method, "#{key}" do
+                                "\e[#{value}m#{self.to_s}\e[0m"
+                        end
+                end
+        end
+
+        def self.colors
+                @@colors_hash.keys
+        end
+
+        def self.sample_colors
+                @@colors_hash.each do |color, _|
+                        puts "This is #{color}.".send(color)
+                end
+        end
+end
+
+String.create_colors
+String.colors
+String.sample_colors
